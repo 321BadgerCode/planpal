7a1c7b29efca5f5f930968053bf3d87ad5591a35
Noah Lerner <126131243+Lernej@users.noreply.github.com>
2026-03-08 21:36:53 -0400

    Added back button to dashboard

--- frontend/src/dashboard/Dashboard.tsx
+++ frontend/src/dashboard/Dashboard.tsx
@@ -1,6 +1,9 @@
 const Dashboard = () => {
 	return (
 		<div className="w-screen h-screen text-white flex items-center justify-center text-2xl">
+			<a href="/">
+				<div className="fixed left-1 top-1 text-white">{"<"}</div>
+			</a>
 			<div>Welcome to your Dashboard</div>
 		</div>
 	);
